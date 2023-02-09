b7fded0fa25d804f4bb2d970f1d3ecd9653a37e8
Wyatt Allan <wyatt@site-source.com>
2023-02-09 09:25:35 -0700

    Added skip and take route for not having to load all of them and instead just a small amount at a time and also a count route to get the amount

--- routes/recentSubmissions.js
+++ routes/recentSubmissions.js
@@ -3,10 +3,66 @@ const { PrismaClient } = require("@prisma/client");
 
 const { user, recentSubmissions } = new PrismaClient();
 
-router.get("/", async (req, res) => {
-  const submissions = await recentSubmissions.findMany({
-    take: 20,
-  });
+router.get("/browse/:skip/:take", async (req, res) => {
+  try {
+    const submissions = await recentSubmissions.findMany({
+      skip: +req.params.skip,
+      take: +req.params.take,
+      orderBy: {
+        id: "desc",
+      },
+    });
+
+    const mappedSubmissions = submissions.map(async (submission) => {
+      const userSubmission = await user.findUnique({
+        where: {
+          id: submission.submissions.userId,
+        },
+        select: {
+          id: true,
+          username: true,
+        },
+      });
+      return {
+        id: submission.id,
+        user: userSubmission,
+        post: submission,
+      };
+    });
+
+    const resolvedSubmissions = await Promise.all(mappedSubmissions);
+
+    return res.status(200).json(resolvedSubmissions);
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "There was an issue with fetching the posts.", error });
+  }
+});
+
+router.get("/count", async (req, res) => {
+  try {
+    const count = await recentSubmissions.count();
+    return res.status(200).json(count);
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "Could not get the count of the submissions", error });
+  }
+});
+
+router.get("/:page", async (req, res) => {
+  console.log(req.params.page);
+  let submissions;
+  if (req.params.page === "home") {
+    submissions = await recentSubmissions.findMany({
+      take: 20,
+    });
+  }
+  if (req.params.page === "browse") {
+    submissions = await recentSubmissions.findMany();
+  }
+
   const mappedSubmissions = submissions.map(async (submission) => {
     const userSubmission = await user.findUnique({
       where: {
@@ -25,15 +81,6 @@ router.get("/", async (req, res) => {
   });
 
   const resolvedSubmissions = await Promise.all(mappedSubmissions);
-
-  // while (resolvedSubmissions.length > 20) {
-  //   const removedSubmission = resolvedSubmissions.shift();
-  //   await recentSubmissions.delete({
-  //     where: {
-  //       id: removedSubmission.id,
-  //     },
-  //   });
-  // }
 
   res.json(resolvedSubmissions.reverse());
 });
