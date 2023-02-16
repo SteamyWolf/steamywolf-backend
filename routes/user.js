cca7d2ffecbdcfcb04feaa0c7cf5cc21908a6874
Wyatt Allan <wyatt@site-source.com>
2023-02-16 11:14:39 -0700

    Forgot password. Reset Password. and verification on new token added

--- routes/user.js
+++ routes/user.js
@@ -2,8 +2,135 @@ const router = require("express").Router();
 const { PrismaClient } = require("@prisma/client");
 const bcrypt = require("bcrypt");
 const nodemailer = require("nodemailer");
+const jwt = require("jsonwebtoken");
 
 const { user } = new PrismaClient();
+
+const transporter = nodemailer.createTransport({
+  service: "hotmail",
+  auth: {
+    user: "steamywolf-website@outlook.com",
+    pass: process.env.NODEMAILER_PASS,
+  },
+});
+
+router.post("/forgot-password", async (req, res) => {
+  const { email } = req.body;
+  // Make sure the email does exist in the database
+  try {
+    const foundUser = await user.findUnique({
+      where: {
+        email: email,
+      },
+    });
+    if (foundUser.email === email) {
+      const jwt_secret = process.env.SECRET_JWT_KEY + foundUser.password;
+      const payload = {
+        email: foundUser.email,
+        id: foundUser.id,
+      };
+      const token = jwt.sign(payload, jwt_secret, { expiresIn: "30m" });
+      const link = `http://localhost:4200/reset-password/${foundUser.id}/${token}`;
+      const options = {
+        from: "steamywolf-website@outlook.com",
+        to: foundUser.email,
+        subject: "SteamyWolf: Reset Password Request",
+        html: `
+          <p>It looks like you have sent a request to reset your password.</p><br>
+          <p>Please click on the link below to initiate the password reset process</p>
+          <a href="${link}">${link}</a>
+        `,
+      };
+
+      transporter.sendMail(options, (err, info) => {
+        if (err) {
+          return res.status(500).json({
+            message: "Error trying to send password request email",
+            error,
+          });
+        }
+        return res
+          .status(200)
+          .json({ message: "Email sent successfully", info });
+      });
+    }
+  } catch (error) {
+    res.status(401).json({ message: "Email not found in registry", error });
+  }
+});
+
+router.get("/reset-password/:id/:token", async (req, res) => {
+  const { id, token } = req.params;
+
+  console.log("id", id, "token", token);
+
+  // Check if this id exists in the database
+  let foundUser;
+  let secret;
+  try {
+    foundUser = await user.findUnique({
+      where: {
+        id: +id,
+      },
+    });
+    secret = process.env.SECRET_JWT_KEY + foundUser.password;
+  } catch (error) {
+    return res
+      .status(401)
+      .json({ message: "Could not find user in database", error });
+  }
+
+  try {
+    const payload = jwt.verify(token, secret);
+    return res.status(200).json({
+      message: "Token verified. User may proceed to reset password",
+      payload,
+    });
+  } catch (error) {
+    return res.status(400).json({
+      message:
+        "Could not verify the token for resetting the password or it has expired",
+      error,
+    });
+  }
+});
+
+router.post("/reset-password-request", async (req, res) => {
+  const { id, password } = req.body;
+
+  let hashedPassword;
+  try {
+    hashedPassword = await bcrypt.hash(password, 10);
+  } catch (error) {
+    return res.status(500).json({
+      message: "There was an issue creating that password. Please try again",
+      error,
+    });
+  }
+
+  try {
+    const foundUser = await user.update({
+      where: {
+        id: +id,
+      },
+      data: {
+        password: hashedPassword,
+      },
+      select: {
+        id: true,
+        username: true,
+      },
+    });
+    return res
+      .status(200)
+      .json({ message: "Successfully updated user password", foundUser });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Error when trying to update the user password",
+      error,
+    });
+  }
+});
 
 router.get("/", async (req, res) => {
   const users = await user.findMany({
@@ -18,14 +145,6 @@ router.get("/", async (req, res) => {
 });
 
 router.post("/email", async (req, res) => {
-  const transporter = nodemailer.createTransport({
-    service: "hotmail",
-    auth: {
-      user: "steamywolf-website@outlook.com",
-      pass: process.env.NODEMAILER_PASS,
-    },
-  });
-
   const options = {
     from: "steamywolf-website@outlook.com",
     to: req.body.email,
