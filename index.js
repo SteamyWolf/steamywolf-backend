4a8197bab589a965d7490c6357e4e9f394e26166
Wyatt Allan <wyatthunterallan@gmail.com>
2025-02-12 09:01:58 -0700

    Cors updated

--- index.js
+++ index.js
@@ -22,8 +22,13 @@ const app = express();
 
 const PORT = process.env.PORT || 4000;
 
+const corsOptions = {
+  credentials: true,
+  origin: process.env.NODE_ENV === 'production' ? process.env.PROD_ORIGIN : process.env.DEV_ORIGIN,
+};
+
 app.use(cookieParser());
-app.use(cors({ credentials: true, origin: "http://localhost:4200" }));
+app.use(cors(corsOptions));
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb" }));
 app.use("/api/user", userRoute);
